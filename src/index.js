e9c35bafd38953f69fe84537c79735d25938de37
mondik58 <vadym.baidak@syndicode.com>
2021-12-23 12:01:51 +0200

    test

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const client = createClient({ url: API_URL });
 ReactDOM.render(
   <React.StrictMode>
     <Provider value={client}>
-      Hello there!
+      Hello
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
