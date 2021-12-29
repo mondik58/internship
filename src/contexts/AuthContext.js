f217c2ff54ce005819049a9c6476eb3fe3ad5ecb
mondik58 <vadym.baidak@syndicode.com>
2021-12-29 11:19:39 +0200

    add auth context

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,20 @@
+import React, {useContext} from "react";
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
+
+
+const AuthProvider = ({children}) => {
+  const value = '';
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthProvider;
+
