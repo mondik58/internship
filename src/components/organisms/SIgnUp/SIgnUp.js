66855eee2c37372111d0d071c97b0d676bc84f38
mondik58 <vadym.baidak@syndicode.com>
2022-01-04 11:02:17 +0200

    minor changes

--- src/components/organisms/SIgnUp/SIgnUp.js
+++ src/components/organisms/SIgnUp/SIgnUp.js
@@ -1,6 +1,6 @@
-import { Grid, TextField, Container } from '@mui/material';
+import { Grid, Container } from '@mui/material';
+import { makeStyles } from '@mui/material';
 import React from 'react';
-import { useState } from 'react';
 import { Formik, Form} from 'formik';
 import * as Yup from 'yup';
 import TextfieldWrapper from '../../atoms/TextFieldWrapper/TextFieldWrapper';
@@ -12,6 +12,14 @@ const INITIAL_FORM_STATE = {
   email: '',
   password: ''
 }
+
+// const useStyles = makeStyles( {
+//   item: {
+//     '& .MuiGrid-item': {
+//       width: '80%'
+//     }
+//   }
+// })
 
 const regExpPass = new RegExp(/^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/);
 const regExpEm = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
@@ -32,13 +40,14 @@ const FORM_VALIDATION = Yup.object().shape({
 })
 
 const Signup = () => {
+  // const classes = useStyles();
 
   return (
-      <Container
-        maxWidth="md"
-      >
-        <Grid item xs={12}
+      
+        <Grid container
               >
+            <Container maxWidth="xs">
+
           <Formik
           initialValues={{
             ...INITIAL_FORM_STATE
@@ -53,32 +62,33 @@ const Signup = () => {
                     direction="column"
                     justifyContent="center"
                     alignItems="center"
-                    spacing={2}>
-                <Grid item xs={12}>
+                    spacing={2}
+                    sx={{marginTop: "100px"}}>
+                <Grid sx={{width: '100%'}} item xs={12}>
                   <TextfieldWrapper 
                     name="firstName"
                     label="First Name"
-                  />
+                    />
                 </Grid>
-              <Grid item md={12}>
+              <Grid sx={{width: '100%'}} item md={12}>
               <TextfieldWrapper 
                     name="lastName"
                     label="Last Name"
-                  />
+                    />
               </Grid>
-                <Grid item xs={12}>
+                <Grid sx={{width: '100%'}} item xs={12}>
                   <TextfieldWrapper 
                       name="email"
                       label="Email"
-                    />
+                      />
                 </Grid>
-                <Grid item xs={12}>
+                <Grid sx={{width: '100%'}} item xs={12}>
                   <TextfieldWrapper 
                         name="password"
                         label="Password"
-                    />
+                        />
                 </Grid>
-                <Grid item xs={12}>
+                <Grid sx={{width: '100%', marginTop: '30px'}} item xs={12}>
                   <ButtonWrapper>
                     SIGN UP
                   </ButtonWrapper>
@@ -86,8 +96,8 @@ const Signup = () => {
               </Grid>
             </Form>
           </Formik>
+          </Container>
         </Grid>
-      </Container>
   );
 }
 
